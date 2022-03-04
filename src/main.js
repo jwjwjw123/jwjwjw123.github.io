c9e6fe316ad04124c070edee400a1a753a3cb465
samjingwen <samjingwen@u.nus.edu>
2022-03-04 14:35:47 +0800

    minor updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-function updateYear(){
-    let date = new Date();
-    let year = date.getFullYear();
-    document.querySelector('#copyright').innerHTML = "Copyright &copy; " + year + " Sam Jing Wen";
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    updateYear();
-});
